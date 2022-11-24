a066264aa015aea49068016bbf91d8357b14a450
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-11-24 21:49:49 +0600

    react hot toast added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Navbar = () => {
+    const { user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err));
+    }
 
     const menuItems = <React.Fragment>
     <li><Link to="/">Home</Link></li>
   
     <li><Link to="/">About</Link></li>
-    <li><Link to="/login">Login</Link></li>
-    <li><Link to="/">Logout</Link></li>  
+
+    {user?.uid ?
+            <>               
+                <li><button onClick={handleLogOut}>Sign Out</button></li>
+            </>
+            : <li><Link to="/login">Login</Link></li>
+        }
+    
 </React.Fragment>
 
     return (
