1a65904b7dc4e200b9ccd1569eac2d0b3765fab8
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-11-24 20:47:57 +0600

    login page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,111 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+
+
 
 const Login = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm()
+    const {signIn, googleSignIn,  resetPassword} = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+    // const [loginUserEmail, setLoginUserEmail] = useState('');
+    // const [token] = useToken(loginUserEmail);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
+
+   
+
+    const handleLogin = data => {
+        console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+        .then(result=> {
+            const user = result.user;
+            console.log(user);
+            // setLoginUserEmail(data.email);
+        })
+        .catch(error => {
+            console.log(error.message)
+            setLoginError(error.message)
+
+        });
+    }
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err =>console.error(err));
+    }
+    
+    const handleResetPassword =(data)=>{
+        console.log(data.email);
+        resetPassword()
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err))
+    }
+
+
     return (
-        <div>
-            <h3>This is form Login</h3>
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type='text'
+                         {...register("email", {
+                            required:"Email Address is required"
+                        })} 
+                         className="input input-bordered w-full max-w-xs" />
+                           {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type='password'
+                         {...register("password", {
+                            required:"Password is required",
+                            minLength: {
+                                value:5,
+                                message: 'Password must have at least 5 character'
+                            }
+                        })}                       
+
+                          className="input input-bordered w-full max-w-xs" />
+                          
+                        <label className="label">
+                           <button onClick={handleResetPassword}> <span className="label-text">Forget Password?</span></button>
+                        </label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+
+
+                    </div>
+                    <input className='btn btn-primary w-full' value="Login" type="submit" />
+                    <div>
+                        {
+                           loginError && <p className='text-red-600'>{loginError} </p>  
+                        }
+                    </div>
+                </form>
+                <p>New to BikeMart? <Link className='text-white font-bold' to="/signup">Create an Account</Link></p>
+                <div className="divider">OR</div>
+                <button onClick={handleGoogleSignIn} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Routes } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
