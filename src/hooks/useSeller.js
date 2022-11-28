91f37bb4f99d9162208b8193cc443e9e8f89feeb
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-11-28 22:52:23 +0600

    custom hook created for seller dashboard

---
+++ src/hooks/useSeller.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+const useSeller = (email) => {
+    const [isSeller, setIsSeller] = useState(false);
+    const [isSellerLoading, SetIsSellerLoading] = useState(true);
+    useEffect(() => {
+        if(email){
+            fetch(`http://localhost:5000/users/seller/${email}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setIsSeller(data.isSeller)
+                SetIsSellerLoading(false);
+            })
+        }
+    }, [email])
+    
+ 
+    return [isSeller, isSellerLoading]
+};
+
+export default useSeller;
